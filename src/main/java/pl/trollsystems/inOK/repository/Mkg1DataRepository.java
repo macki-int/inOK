842232ad7c9463b27cc5a31b67c081d54005d0f9
macki-int <macki@int.pl>
2022-08-08 13:14:20 +0200

    add annotation @Repository in interface Mkg1DataRepository

--- src/main/java/pl/trollsystems/inOK/repository/Mkg1DataRepository.java
+++ src/main/java/pl/trollsystems/inOK/repository/Mkg1DataRepository.java
@@ -1,7 +1,9 @@
 package pl.trollsystems.inOK.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.trollsystems.inOK.model.Mkg1Data;
 
+@Repository
 public interface Mkg1DataRepository extends JpaRepository<Mkg1Data, Long> {
 }
