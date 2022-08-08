40d75117d0ae7d3900b7fc34aa7ea93791297504
macki-int <macki@int.pl>
2022-08-08 13:17:58 +0200

    add restcontroller Mkg1DaraController class

---
+++ src/main/java/pl/trollsystems/inOK/controller/Mkg1DataController.java
@@ -0,0 +1,27 @@
+package pl.trollsystems.inOK.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.trollsystems.inOK.model.Mkg1Data;
+import pl.trollsystems.inOK.repository.Mkg1DataRepository;
+
+@RestController
+@RequestMapping("/mkg1data")
+public class Mkg1DataController {
+    private Mkg1DataRepository mkg1DataRepository;
+
+    @Autowired
+    public Mkg1DataController(Mkg1DataRepository mkg1DataRepository) {
+        this.mkg1DataRepository = mkg1DataRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> findAll(){
+//           mkg1DataRepository.findAll();
+
+        return ResponseEntity.ok().body(mkg1DataRepository.findAll());
+    }
+}
