32efeaed5caa2dc9884a5d7700d7ba531d814abb
macki-int <macki@int.pl>
2022-08-08 11:47:41 +0200

    add Mkg1Dat repository interface

---
+++ src/main/java/pl/trollsystems/inOK/repository/Mkg1DataRepository.java
@@ -0,0 +1,7 @@
+package pl.trollsystems.inOK.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.trollsystems.inOK.model.Mkg1Data;
+
+public interface Mkg1DataRepository extends JpaRepository<Mkg1Data, Long> {
+}
