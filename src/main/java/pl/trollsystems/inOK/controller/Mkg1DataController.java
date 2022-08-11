70596853c78a70a6c1542235639a970487d4c46f
macki-int <macki@int.pl>
2022-08-11 15:59:16 +0200

    remove import import pl.trollsystems.inOK.model.Mkg1Data;

--- src/main/java/pl/trollsystems/inOK/controller/Mkg1DataController.java
+++ src/main/java/pl/trollsystems/inOK/controller/Mkg1DataController.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.trollsystems.inOK.model.Mkg1Data;
 import pl.trollsystems.inOK.repository.Mkg1DataRepository;
 
 @RestController
