62bab15c4a8d77578d7a04071d2bd6dadf475834
Marek <macki@int.pl>
2022-08-25 16:01:04 +0200

    add findAllByPlace method in Mkg1DataRepository

--- src/main/java/pl/trollsystems/inOK/repository/Mkg1DataRepository.java
+++ src/main/java/pl/trollsystems/inOK/repository/Mkg1DataRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.trollsystems.inOK.model.Mkg1Data;
 
+import java.util.List;
+
 @Repository
 public interface Mkg1DataRepository extends JpaRepository<Mkg1Data, Long> {
+    List<Mkg1Data> findAllByPlace(String place);
 }
