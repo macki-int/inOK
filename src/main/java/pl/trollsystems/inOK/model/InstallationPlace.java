9928929a9e0b4fa924c3a76fbdf4a29b95032e97
Marek <macki@int.pl>
2022-08-30 14:40:45 +0200

    change name in @Table annotation on installation_places

--- src/main/java/pl/trollsystems/inOK/model/InstallationPlace.java
+++ src/main/java/pl/trollsystems/inOK/model/InstallationPlace.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "installation_sites")
+@Table(name = "installation_places")
 public class InstallationPlace {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
