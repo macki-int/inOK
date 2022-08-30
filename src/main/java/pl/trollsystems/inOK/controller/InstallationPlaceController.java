2877d9c91d42b61976c2393d025cb5ff62973559
Marek <macki@int.pl>
2022-08-30 13:48:53 +0200

    add InstallationPlaceController class

---
+++ src/main/java/pl/trollsystems/inOK/controller/InstallationPlaceController.java
@@ -0,0 +1,10 @@
+package pl.trollsystems.inOK.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/installation-place")
+public class InstallationPlaceController {
+    
+}
