5fdadcb98a3b0ffb2e052d93122c319d1340b23c
Marek <macki@int.pl>
2022-08-30 13:55:11 +0200

    add getMapping method findAll in InstallationPlaceController

--- src/main/java/pl/trollsystems/inOK/controller/InstallationPlaceController.java
+++ src/main/java/pl/trollsystems/inOK/controller/InstallationPlaceController.java
@@ -1,6 +1,8 @@
 package pl.trollsystems.inOK.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.trollsystems.inOK.repository.InstallationPlaceRepository;
@@ -15,5 +17,8 @@ public class InstallationPlaceController {
         this.installationPlaceRepository = installationPlaceRepository;
     }
 
-    
+    @GetMapping
+    public ResponseEntity<?> findAll(){
+        return ResponseEntity.ok().body(installationPlaceRepository.findAll());
+    }
 }
