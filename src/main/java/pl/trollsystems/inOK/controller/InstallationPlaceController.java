7f77b9f43ac6882159edfb19bd712f9090c3cfd2
Marek <macki@int.pl>
2022-08-30 13:51:07 +0200

    add dependency injection i InstallationPlaceController

--- src/main/java/pl/trollsystems/inOK/controller/InstallationPlaceController.java
+++ src/main/java/pl/trollsystems/inOK/controller/InstallationPlaceController.java
@@ -1,10 +1,19 @@
 package pl.trollsystems.inOK.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.trollsystems.inOK.repository.InstallationPlaceRepository;
 
 @RestController
 @RequestMapping("/installation-place")
 public class InstallationPlaceController {
+    private InstallationPlaceRepository installationPlaceRepository;
+
+    @Autowired
+    public InstallationPlaceController(InstallationPlaceRepository installationPlaceRepository) {
+        this.installationPlaceRepository = installationPlaceRepository;
+    }
+
     
 }
