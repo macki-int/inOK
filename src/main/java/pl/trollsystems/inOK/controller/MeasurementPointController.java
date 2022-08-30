754b835182368c7a24af3dc299d9142fb49b98b3
Marek <macki@int.pl>
2022-08-30 14:55:39 +0200

    add restcontroller MeasurementPointController

---
+++ src/main/java/pl/trollsystems/inOK/controller/MeasurementPointController.java
@@ -0,0 +1,10 @@
+package pl.trollsystems.inOK.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/measurement-points")
+public class MeasurementPointController {
+
+}
