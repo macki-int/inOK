8e9300c188c0d7134c03e41f2a4e84807c0366f3
Marek <macki@int.pl>
2022-08-30 14:57:12 +0200

    add dependency injection MeasurementPointRepository in MeasurementPointController

--- src/main/java/pl/trollsystems/inOK/controller/MeasurementPointController.java
+++ src/main/java/pl/trollsystems/inOK/controller/MeasurementPointController.java
@@ -1,10 +1,17 @@
 package pl.trollsystems.inOK.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.trollsystems.inOK.repository.MeasurementPointRepository;
 
 @RestController
 @RequestMapping("/measurement-points")
 public class MeasurementPointController {
+    private MeasurementPointRepository measurementPointRepository;
 
+    @Autowired
+    public MeasurementPointController(MeasurementPointRepository measurementPointRepository) {
+        this.measurementPointRepository = measurementPointRepository;
+    }
 }
