c1986a248b50bad9ba5c38e61bd1c0122e4e5f71
Marek <macki@int.pl>
2022-09-01 11:25:17 +0200

    add findAll method in MeasurmentPointController class

--- src/main/java/pl/trollsystems/inOK/controller/MeasurementPointController.java
+++ src/main/java/pl/trollsystems/inOK/controller/MeasurementPointController.java
@@ -1,6 +1,8 @@
 package pl.trollsystems.inOK.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.trollsystems.inOK.repository.MeasurementPointRepository;
@@ -13,5 +15,10 @@ public class MeasurementPointController {
     @Autowired
     public MeasurementPointController(MeasurementPointRepository measurementPointRepository) {
         this.measurementPointRepository = measurementPointRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> findAll(){
+        return ResponseEntity.ok().body(measurementPointRepository.findAll());
     }
 }
